082831760442117a8183bada3d4796209625529e
Bartek Rycharski <bartlomiej.rycharski@gmail.com>
2011-12-04 13:53:29 +0100

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default :from => "VideoQuiz <#{@win.quiz.company_email}>"
-   #TODO: adres maila
+ 
 
   def reward(win_id)
     @win = Win.find(win_id)
 
     mail :to => @win.email,
-         :subject => "Kod rabatowy w VideoQuizie #{@win.quiz.name}"
+         :subject => "Kod rabatowy w VideoQuizie #{@win.quiz.name}",
+	:from => "VideoQuiz <#{@win.quiz.company_email}>"
   end
 end
